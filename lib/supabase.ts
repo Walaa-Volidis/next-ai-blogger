dec172341cc0bcd030b48118a0ad8a9df89f2773
Walaa VOILIDAS <walaavolidis@gmail.com>
2025-11-29 12:13:21 +0200

    change form and supabase

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,9 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
+import { SERVER_SETTINGS } from '@/app/settings'
 
-const supabaseUrl = process.env.SUPABASE_URL as string
-const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY as string
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(
+  SERVER_SETTINGS.supabaseUrl,
+  SERVER_SETTINGS.supabaseServiceRoleKey
+)
 
 export async function getBlogById(id: number) {
   const { data, error } = await supabase
